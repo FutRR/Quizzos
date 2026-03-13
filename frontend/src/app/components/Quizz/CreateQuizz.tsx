7de341d556ada957a0305c4915205db73323fb82
MrLylas <delmas.denilson.pro@gmail.com>
2026-03-13 17:56:42 +0100

    feat: add CreateQuizz component and integrate it into NewQuiz page

---
+++ frontend/src/app/components/Quizz/CreateQuizz.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CreateQuizz() {
+    return (
+        <div>
+            <h1>Créer un quizz</h1>
+        </div>
+    );
+}
---
+++ frontend/src/app/quizzes/new/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import CreateQuizz from "@/app/components/Quizz/CreateQuizz";
+
+export default function NewQuiz() {
+    return (
+        <div>
+            <h1>Nouveau Quiz</h1>
+            <CreateQuizz />
+        </div>
+    );
+}
