7a87c602f7d8149eed0025ff09b921bab372c5dc
FutRR <futterermaxime@gmail.com>
2026-03-13 14:02:52 +0100

    refactor: remove unnecessary blank line in Profile component and tidy up userTypes interface

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,6 +1,5 @@
 import RegisterForm from "../components/Register/RegisterForm";
 
-
 export default function Profile() {
     return (
         <div>
--- frontend/src/app/types/userTypes.ts
+++ frontend/src/app/types/userTypes.ts
@@ -1,6 +1,6 @@
 export interface BaseUser {
     id?: string;
-    userName: string;   
+    userName: string;
     createdAt: Date;
 }
 
