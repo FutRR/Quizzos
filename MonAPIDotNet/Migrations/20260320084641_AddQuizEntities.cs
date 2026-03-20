8a7a502fa35761efe7ad20289bbdac7313104e5b
FutRR <futterermaxime@gmail.com>
2026-03-20 09:48:37 +0100

    feat: add migration for Quiz entities and update XML documentation

---
+++ MonAPIDotNet/Migrations/20260320084641_AddQuizEntities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonAPIDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
