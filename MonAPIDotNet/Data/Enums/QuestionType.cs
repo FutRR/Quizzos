62deb90d46d22e2109c2201b6f05283d1ccfb5e6
FutRR <futterermaxime@gmail.com>
2026-03-21 09:24:30 +0100

    feat: add JsonStringEnumConverter to DifficultyType and QuestionType enums

--- MonAPIDotNet/Data/Enums/QuestionType.cs
+++ MonAPIDotNet/Data/Enums/QuestionType.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace MonAPIDotNet.Data
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum QuestionType
     {
         MultipleChoice,
