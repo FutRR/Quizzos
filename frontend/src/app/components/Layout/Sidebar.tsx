5de92dc35a957d0c166d866ae2771a261d398d8b
FutRR <futterermaxime@gmail.com>
2026-04-01 16:25:48 +0200

    chore: update version number to v0.2 in Sidebar component

--- frontend/src/app/components/Layout/Sidebar.tsx
+++ frontend/src/app/components/Layout/Sidebar.tsx
@@ -262,7 +262,7 @@ export default function Sidebar() {
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <span>Quizzos v0.1</span>
+            <span>Quizzos v0.2</span>
           </div>
         </div>
       </aside>
