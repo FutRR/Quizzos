8c8666e86703c11e8a67606db5687f48cc0fa1b7
FutRR <futterermaxime@gmail.com>
2026-04-24 10:11:54 +0200

    fix: update button color in Profile component for consistency

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -25,7 +25,7 @@ export default function Profile() {
           </div>
         )}
         <button
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 w-1/4 rounded"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-1/4 rounded"
           onClick={toggle}
         >
           Modifier
